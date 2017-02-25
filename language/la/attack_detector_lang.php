4d23a42cade1e4d9abb00d8e636bc883d9d6cc1d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:33:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'Attack Detector';
+$lang['attack_detector_app_description'] = 'Attack Detector lustrat peccata vestra across authenticas declarandas ratio pro diversis meritis installed in vestri ratio. Si limen ventum est defectus, ratio tentatio sepiam app.';
+$lang['attack_detector_log'] = 'Log';
+$lang['attack_detector_rule'] = 'Lex';
+$lang['attack_detector_rules'] = 'Rules';
+$lang['attack_detector_rule_name'] = 'nomine';
+$lang['attack_detector_ban_time'] = 'Ban Tempus';
+$lang['attack_detector_rule_invalid'] = 'Regula est invalidum.';
