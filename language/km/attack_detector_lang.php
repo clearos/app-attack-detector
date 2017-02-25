8227761a5bbe889d093752d72089dbffd35092b0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:33:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'ការវាយប្រហារចាប់';
+$lang['attack_detector_app_description'] = 'ការវាយប្រហារស្កេនប្រព័ន្ធចាប់របស់អ្នកសម្រាប់ការបរាជ័យការផ្ទៀងផ្ទាត់ភាពត្រឹមត្រូវនៅទូទាំងប្រភេទផ្សេងគ្នានៃការផ្តល់សេវាដែលបានដំឡើងនៅលើប្រព័ន្ធរបស់អ្នក។ ប្រសិនបើកំរិតបរាជ័យនេះត្រូវបានឈានដល់, កម្មវិធីនេះនឹងរារាំងប្រព័ន្ធការវាយប្រហារនេះ។';
+$lang['attack_detector_log'] = 'កំណត់ហេតុ';
+$lang['attack_detector_rule'] = 'វិធាន';
+$lang['attack_detector_rules'] = 'ក្បួន';
+$lang['attack_detector_rule_name'] = 'ឈ្មោះ';
+$lang['attack_detector_ban_time'] = 'លោកបានគីមូម៉ោង';
+$lang['attack_detector_rule_invalid'] = 'វិធានមិនត្រឹមត្រូវ។';
