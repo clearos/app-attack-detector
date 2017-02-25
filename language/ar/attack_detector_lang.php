44f9d02004f5ecba941cdd7a42dc557f4d261051
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:28:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'الكاشف هجوم';
+$lang['attack_detector_app_description'] = 'الكاشف هجوم بفحص النظام الخاص بك لفشل المصادقة عبر أنواع مختلفة من الخدمات المثبتة على النظام الخاص بك. إذا تم الوصول إلى عتبة فشل، فإن التطبيق منع النظام المهاجمة.';
+$lang['attack_detector_log'] = 'سجل';
+$lang['attack_detector_rule'] = 'قاعدة';
+$lang['attack_detector_rules'] = 'قواعد';
+$lang['attack_detector_rule_name'] = 'اسم';
+$lang['attack_detector_ban_time'] = 'بان الوقت';
+$lang['attack_detector_rule_invalid'] = 'حكم غير صالحة.';
