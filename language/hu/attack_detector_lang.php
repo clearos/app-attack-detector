2aae0217cbf2397b8253be9ccc62db72bd570da0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:31:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'Attack Detector';
+$lang['attack_detector_app_description'] = 'Attack Detector átvizsgálja a rendszer hitelesítési hibák között különböző típusú szolgáltatások telepítve van a rendszeren. Ha a hiba küszöb elérésekor az alkalmazás blokkolja a támadó rendszer.';
+$lang['attack_detector_log'] = 'Bejelentkezés';
+$lang['attack_detector_rule'] = 'Szabály';
+$lang['attack_detector_rules'] = 'szabályok';
+$lang['attack_detector_rule_name'] = 'Név';
+$lang['attack_detector_ban_time'] = 'Ban idő';
+$lang['attack_detector_rule_invalid'] = 'Szabály érvénytelen.';
