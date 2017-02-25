656aab78d047ee1a4cb11f955031ac707b4047a0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:28:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'Erasoa Detector';
+$lang['attack_detector_app_description'] = 'Erasoa Detector zure sistema sisteman instalatuta zerbitzu mota ezberdinak zeharkatuz autentifikazio porrotak bilatzen hasiko da. porrota atarian iritsi bada, aplikazioa erasotzeko sistema blokeatu egingo.';
+$lang['attack_detector_log'] = 'Saioa';
+$lang['attack_detector_rule'] = 'Arau';
+$lang['attack_detector_rules'] = 'arauak';
+$lang['attack_detector_rule_name'] = 'izena';
+$lang['attack_detector_ban_time'] = 'Ban Time';
+$lang['attack_detector_rule_invalid'] = 'Araua ez da zuzena.';
