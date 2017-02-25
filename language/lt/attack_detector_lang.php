e080727f2963abfe34f03c54e6f638756ac0a8e3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:33:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'ataka detektorius';
+$lang['attack_detector_app_description'] = 'Ataka detektorius nuskaito jūsų sistemą autentifikacijos nesėkmių įvairiuose tipų įdiegtas Jūsų sistemoje paslaugas. Jei pasiekiama nepakankamumas riba, app bus blokuoti puola sistemą.';
+$lang['attack_detector_log'] = 'Prisijungti';
+$lang['attack_detector_rule'] = 'taisyklė';
+$lang['attack_detector_rules'] = 'Taisyklės';
+$lang['attack_detector_rule_name'] = 'vardas';
+$lang['attack_detector_ban_time'] = 'Ban laikas';
+$lang['attack_detector_rule_invalid'] = 'Taisyklė negalioja.';
