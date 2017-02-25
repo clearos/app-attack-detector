87b268af7a4b188e42e3844868381f959553cc58
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:39:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'Attack oluwari';
+$lang['attack_detector_app_description'] = 'Attack oluwari léraléra rẹ eto fun ìfàṣẹsí ikuna kọja orisirisi orisi ti awọn iṣẹ ti fi sori ẹrọ lori eto rẹ. Ti o ba ti awọn ikuna ala ni ami, awọn app yoo dènà awọn bàa eto.';
+$lang['attack_detector_log'] = 'Wọle';
+$lang['attack_detector_rule'] = 'ofin';
+$lang['attack_detector_rules'] = 'ofin';
+$lang['attack_detector_rule_name'] = 'Name';
+$lang['attack_detector_ban_time'] = 'Ban Time';
+$lang['attack_detector_rule_invalid'] = 'Ofin ni invalid.';
