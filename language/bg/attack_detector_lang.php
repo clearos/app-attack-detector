eb69e1eb7ef19c42d0bfebb910ca5289887b91e5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:29:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bg/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'атака Detector';
+$lang['attack_detector_app_description'] = 'Атака детектор сканира вашата система за неуспехите на автентичност в целия различни видове инсталирани на системата ви услуги. Ако не се стигне прага на провал, приложението ще блокира системата атакува.';
+$lang['attack_detector_log'] = 'Вход';
+$lang['attack_detector_rule'] = 'правило';
+$lang['attack_detector_rules'] = 'правилник';
+$lang['attack_detector_rule_name'] = 'име';
+$lang['attack_detector_ban_time'] = 'Бан Time';
+$lang['attack_detector_rule_invalid'] = 'Правило е невалиден.';
