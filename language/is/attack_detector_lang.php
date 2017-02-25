be7c880738609a76a99013b11e180dd6d0e4c65d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:32:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'árás Detector';
+$lang['attack_detector_app_description'] = 'Árás Detector skannar vélina þína til auðkenningar mistök yfir ýmiss konar þjónustu uppsett á vélinni þinni. Ef bilun marki er náð, the app mun loka á ráðast kerfið.';
+$lang['attack_detector_log'] = 'Log';
+$lang['attack_detector_rule'] = 'regla';
+$lang['attack_detector_rules'] = 'reglur';
+$lang['attack_detector_rule_name'] = 'heiti';
+$lang['attack_detector_ban_time'] = 'Ban Time';
+$lang['attack_detector_rule_invalid'] = 'Reglan er ógilt.';
