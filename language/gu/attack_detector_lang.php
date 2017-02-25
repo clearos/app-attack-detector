cc825c30c8a8a7ec8c191141505f20d74a2827d3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:31:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'હુમલો ડીટેક્ટર';
+$lang['attack_detector_app_description'] = 'હુમલો ડીટેક્ટર તમારી સિસ્ટમ પર સ્થાપિત સેવાઓ વિવિધ પ્રકારના સમગ્ર સત્તાધિકરણ નિષ્ફળતાઓ માટે તમારી સિસ્ટમ સ્કેન કરે છે. નિષ્ફળતા થ્રેશોલ્ડ સુધી પહોંચી છે, તો, એપ્લિકેશન હુમલો સિસ્ટમ અવરોધિત કરશે.';
+$lang['attack_detector_log'] = 'પ્રવેશ કરો';
+$lang['attack_detector_rule'] = 'નિયમ';
+$lang['attack_detector_rules'] = 'નિયમો';
+$lang['attack_detector_rule_name'] = 'નામ';
+$lang['attack_detector_ban_time'] = 'પ્રતિબંધ સમય';
+$lang['attack_detector_rule_invalid'] = 'નિયમ અમાન્ય છે.';
