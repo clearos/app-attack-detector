b100a1b6c5295cdf90cd54c82af10030d55a79a9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:31:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'Ανιχνευτής επίθεση';
+$lang['attack_detector_app_description'] = 'Ανιχνευτής επίθεση σαρώνει το σύστημά σας για τις αποτυχίες ελέγχου ταυτότητας σε διάφορα είδη υπηρεσιών εγκατεστημένο στο σύστημά σας. Αν επιτευχθεί το όριο αστοχίας, η εφαρμογή θα μπλοκάρει το σύστημα να επιτεθεί.';
+$lang['attack_detector_log'] = 'Κούτσουρο';
+$lang['attack_detector_rule'] = 'Κανόνας';
+$lang['attack_detector_rules'] = 'κανόνες';
+$lang['attack_detector_rule_name'] = 'Ονομα';
+$lang['attack_detector_ban_time'] = 'Μπαν Ώρα';
+$lang['attack_detector_rule_invalid'] = 'Κανόνα δεν είναι έγκυρο.';
