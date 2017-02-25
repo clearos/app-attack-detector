71afbd7938b39c1dbfa2eb81680df4e0be59f54e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:28:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'Հարձակում հայտնաբերիչ';
+$lang['attack_detector_app_description'] = 'Հարձակում հայտնաբերիչ scans ձեր համակարգը վավերացման համար անհաջողությունների տնտեսության տարբեր տեսակի ծառայությունների տեղադրված է ձեր համակարգը. Եթե ​​ձախողումը շեմը է հասել, որ ծրագիրը պետք է արգելափակել հարձակվում համակարգը:';
+$lang['attack_detector_log'] = 'log';
+$lang['attack_detector_rule'] = 'կանոն';
+$lang['attack_detector_rules'] = 'Օրենքները';
+$lang['attack_detector_rule_name'] = 'անուն';
+$lang['attack_detector_ban_time'] = 'Ban Time';
+$lang['attack_detector_rule_invalid'] = 'Կանոն է անվավեր:';
