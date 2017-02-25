40ee096c611bdfec3003116e1abe318655a2e24a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:32:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'თავდასხმა Detector';
+$lang['attack_detector_app_description'] = 'თავდასხმა Detector დაასკანირებს თქვენს სისტემას ავტორიზაციის წარუმატებლობის სხვადასხვა სახის მომსახურება დაინსტალირებული თქვენს სისტემაში. თუ მარცხი ზღვარი, app დაბლოკოს თავდასხმაში სისტემა.';
+$lang['attack_detector_log'] = 'შესვლა';
+$lang['attack_detector_rule'] = 'წესი';
+$lang['attack_detector_rules'] = 'წესები';
+$lang['attack_detector_rule_name'] = 'სახელი';
+$lang['attack_detector_ban_time'] = 'Ban დრო';
+$lang['attack_detector_rule_invalid'] = 'წესი არასწორია.';
