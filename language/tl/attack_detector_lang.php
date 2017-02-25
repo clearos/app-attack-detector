4f233fd5879f4eafc577dcb3743f039fe7f0d57b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:38:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'Attack Detector';
+$lang['attack_detector_app_description'] = 'Attack Detector ini-scan ang iyong system para pagkabigo authentication buong iba&#39;t-ibang uri ng mga serbisyo na naka-install sa iyong system. Kung ang kabiguan threshold ay naabot, ang app ay i-block ang paglusob system.';
+$lang['attack_detector_log'] = 'Log';
+$lang['attack_detector_rule'] = 'patakaran';
+$lang['attack_detector_rules'] = 'Rules';
+$lang['attack_detector_rule_name'] = 'pangalan';
+$lang['attack_detector_ban_time'] = 'Ban Time';
+$lang['attack_detector_rule_invalid'] = 'Rule ay hindi wasto.';
