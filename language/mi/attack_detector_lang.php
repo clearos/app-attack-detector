44f49f5a77f1da9ecf76e817aef7f60b9c73f947
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:34:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'Detector Attack';
+$lang['attack_detector_app_description'] = 'Attack Detector matawai koutou pūnaha mō rahunga motuhēhēnga puta noa ngā momo o ratonga tāutatia i runga i tou pūnaha. Ki te tae te paepae kore te, te taupānga ka aukati i te pūnaha whawhai.';
+$lang['attack_detector_log'] = 'Rangitaki';
+$lang['attack_detector_rule'] = 'ture';
+$lang['attack_detector_rules'] = 'ture';
+$lang['attack_detector_rule_name'] = 'ingoa';
+$lang['attack_detector_ban_time'] = 'Wā Ban';
+$lang['attack_detector_rule_invalid'] = 'He muhu Ture.';
