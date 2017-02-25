cc0973d392c9cc60d6b07e128841e738643a4cbe
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:32:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'agha Detector';
+$lang['attack_detector_app_description'] = 'Agha Detector enyocha kwa gị usoro maka Nyocha ọdịda gafee iche iche na ụdị ọrụ arụnyere na gị usoro. Ọ bụrụ na odida fọrọ na-ruru, ngwa ga-egbochi ndị agha usoro.';
+$lang['attack_detector_log'] = 'log';
+$lang['attack_detector_rule'] = '-achị';
+$lang['attack_detector_rules'] = 'iwu';
+$lang['attack_detector_rule_name'] = 'aha';
+$lang['attack_detector_ban_time'] = 'Ban Time';
+$lang['attack_detector_rule_invalid'] = '-Achị bụ ghara ịdị irè.';
