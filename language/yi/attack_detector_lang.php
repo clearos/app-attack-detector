5c5025ac271e2eb86d3b47fbd63250979adde688
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:39:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'אַטאַק דעטעקטאָר';
+$lang['attack_detector_app_description'] = 'אַטאַק דעטעקטאָר סקאַנז דיין סיסטעם פֿאַר אָטענטאַקיישאַן פאַילורעס אַריבער פאַרשידן טייפּס פון באַדינונגען אינסטאַלירן אויף אייער סיסטעם. אויב די דורכפאַל שוועל איז ריטשט, די אַפּ וועט פאַרשפּאַרן די קעגן סיסטעם.';
+$lang['attack_detector_log'] = 'קלאָץ';
+$lang['attack_detector_rule'] = 'הערשן';
+$lang['attack_detector_rules'] = 'רולעס';
+$lang['attack_detector_rule_name'] = 'נאָמען';
+$lang['attack_detector_ban_time'] = 'פאַרבאָט Time';
+$lang['attack_detector_rule_invalid'] = 'הערשן איז פאַרקריפּלט.';
