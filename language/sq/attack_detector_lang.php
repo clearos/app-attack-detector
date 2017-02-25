d9dc8eda882f1c8d0d60ead9a3bd8dcc56f5db21
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:28:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'sulmi Detector';
+$lang['attack_detector_app_description'] = 'Sulmi Detector Scans sistemin tuaj për dështimet e vërtetimit të gjithë llojeve të ndryshme të shërbimeve të instaluar në sistemin tuaj. Nëse pragu dështimi është arritur, app do të bllokojë sistemin sulmuar.';
+$lang['attack_detector_log'] = 'Identifikohu';
+$lang['attack_detector_rule'] = 'rregull';
+$lang['attack_detector_rules'] = 'Rregullat';
+$lang['attack_detector_rule_name'] = 'emër';
+$lang['attack_detector_ban_time'] = 'Ban Koha';
+$lang['attack_detector_rule_invalid'] = 'Rregulli është i pavlefshëm.';
