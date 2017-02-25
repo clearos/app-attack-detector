5d855ff32e8eff77da2592a21495a2d25c71261b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:37:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'qalabka weerarkii';
+$lang['attack_detector_app_description'] = 'Qalabka weerarkii uu baaritaanku habka fashilka sugida ee guud ahaan noocyada kala duwan ee adeegyada lagu rakibay on your nidaamka. Haddii marinkii failure la gaaro, app ku xiri doonta nidaamka weerarka.';
+$lang['attack_detector_log'] = 'log';
+$lang['attack_detector_rule'] = 'Rule';
+$lang['attack_detector_rules'] = 'Rules';
+$lang['attack_detector_rule_name'] = 'Magaca';
+$lang['attack_detector_ban_time'] = 'Ban Time';
+$lang['attack_detector_rule_invalid'] = 'Rule waa baadil.';
