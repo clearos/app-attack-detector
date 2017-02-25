f7dda2c558323b0f639e52750a820015269038e1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:31:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'atak Detektè';
+$lang['attack_detector_app_description'] = 'Atak Detektè analiz sistèm ou pou echèk otantifikasyon atravè divès kalite sèvis enstale sou sistèm ou an. Si yo papòt la echèk rive jwenn, app a pral bloke sistèm nan atake.';
+$lang['attack_detector_log'] = 'Log';
+$lang['attack_detector_rule'] = 'règ';
+$lang['attack_detector_rules'] = 'règ';
+$lang['attack_detector_rule_name'] = 'Non';
+$lang['attack_detector_ban_time'] = 'Ban Tan';
+$lang['attack_detector_rule_invalid'] = 'Règ se valab.';
