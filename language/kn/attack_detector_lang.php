43e7148d4ed13fa16456294e65bd5004bc719d70
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:33:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'ಅಟ್ಯಾಕ್ ಡಿಟೆಕ್ಟರ್';
+$lang['attack_detector_app_description'] = 'ಅಟ್ಯಾಕ್ ಡಿಟೆಕ್ಟರ್ ನಿಮ್ಮ ಗಣಕದಲ್ಲಿ ಅನುಸ್ಥಾಪಿಸಲಾದ ಸೇವೆಗಳು ವಿವಿಧ ಅಡ್ಡಲಾಗಿ ದೃಢೀಕರಣ ವೈಫಲ್ಯಗಳನ್ನು ನಿಮ್ಮ ವ್ಯವಸ್ಥೆಯ ಸ್ಕ್ಯಾನ್. ವೈಫಲ್ಯ ಮಿತಿಯನ್ನು ತಲುಪಿದೆ ವೇಳೆ, ಅಪ್ಲಿಕೇಶನ್ ದಾಳಿ ವ್ಯವಸ್ಥೆಯ ನಿರ್ಬಂಧಿಸುತ್ತದೆ.';
+$lang['attack_detector_log'] = 'ಲಾಗ್';
+$lang['attack_detector_rule'] = 'ರೂಲ್';
+$lang['attack_detector_rules'] = 'ನಿಯಮಗಳು';
+$lang['attack_detector_rule_name'] = 'ಹೆಸರು';
+$lang['attack_detector_ban_time'] = 'ಬಾನ್ ಟೈಮ್';
+$lang['attack_detector_rule_invalid'] = 'ರೂಲ್ ಅಮಾನ್ಯವಾಗಿದೆ.';
