95f27deb910c15d80a465d2bd30d56510bc296d1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:35:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'Detector attakk';
+$lang['attack_detector_app_description'] = 'Detector Attakk skanijiet-sistema tiegħek għall-fallimenti awtentikazzjoni madwar diversi tipi ta &#39;servizzi installati fis-sistema tiegħek. Jekk il-limitu falliment jintlaħaq, l-app se jimblokka s-sistema jattakkaw.';
+$lang['attack_detector_log'] = 'Zokk maqtugħ';
+$lang['attack_detector_rule'] = 'regola';
+$lang['attack_detector_rules'] = 'regoli';
+$lang['attack_detector_rule_name'] = 'isem';
+$lang['attack_detector_ban_time'] = 'Ban Ħin';
+$lang['attack_detector_rule_invalid'] = 'Regola huwa invalidu.';
