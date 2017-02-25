3e95a5957ce7b723ee38aa383201ec3303065f73
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:39:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'Synhwyrydd Attack';
+$lang['attack_detector_app_description'] = 'Synhwyrydd Attack sganiau eich system ar gyfer methiannau dilysu ar draws gwahanol fathau o wasanaethau osod ar eich system. Os bydd y trothwy methiant yn cael ei gyrraedd, bydd y app bloc y system ymosod.';
+$lang['attack_detector_log'] = 'log';
+$lang['attack_detector_rule'] = 'rheol';
+$lang['attack_detector_rules'] = 'rheolau';
+$lang['attack_detector_rule_name'] = 'enw';
+$lang['attack_detector_ban_time'] = 'Ban Amser';
+$lang['attack_detector_rule_invalid'] = 'Rheol yn annilys.';
