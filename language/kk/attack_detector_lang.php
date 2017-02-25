c731101f29cc1405ae38949ddf7a3777745ef8fe
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:33:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'Attack детектор';
+$lang['attack_detector_app_description'] = 'Attack детектор Жүйеңізде орнатылған қызметтердің әр түрлі арқылы қате түпнұсқалық жүйені сканерлейді. жеткіліксіздігі шекті жеткен болса, қолданба шабуыл жүйесін блоктайды.';
+$lang['attack_detector_log'] = 'журнал';
+$lang['attack_detector_rule'] = 'ереже';
+$lang['attack_detector_rules'] = 'Ережелер';
+$lang['attack_detector_rule_name'] = 'ат';
+$lang['attack_detector_ban_time'] = 'Пан Time';
+$lang['attack_detector_rule_invalid'] = 'Ереже жарамсыз болып табылады.';
