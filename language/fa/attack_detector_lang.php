ac5c5eaf943938079ae3e4dbd8c67808d56296c6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:35:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'آشکارساز حمله';
+$lang['attack_detector_app_description'] = 'آشکارساز حمله اسکن سیستم خود را برای شکست احراز هویت در میان انواع مختلف خدمات نصب شده بر روی سیستم شما. اگر آستانه شکست رسیده است، این برنامه به سیستم حمله را مسدود کند.';
+$lang['attack_detector_log'] = 'ورود';
+$lang['attack_detector_rule'] = 'قانون';
+$lang['attack_detector_rules'] = 'قوانین';
+$lang['attack_detector_rule_name'] = 'نام';
+$lang['attack_detector_ban_time'] = 'بان کی مون زمان';
+$lang['attack_detector_rule_invalid'] = 'قانون نامعتبر است.';
