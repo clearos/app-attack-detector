8ec9bc83fc07dcb153229af2dea6880cdb4a5938
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:39:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'Attack Detector';
+$lang['attack_detector_app_description'] = 'Attack Detector aqalaze system yakho ukwehluleka ukufakazela ubuqiniso yonkana izinhlobo ezahlukene services efakwe kusistimu yakho. Uma umbundu ukwehluleka kufinyelelwe, uhlelo lokusebenza ngeke ukuvimba uhlelo ehlasela.';
+$lang['attack_detector_log'] = 'Log';
+$lang['attack_detector_rule'] = 'Rule';
+$lang['attack_detector_rules'] = 'Rules';
+$lang['attack_detector_rule_name'] = 'Igama';
+$lang['attack_detector_ban_time'] = 'Ban Isikhathi';
+$lang['attack_detector_rule_invalid'] = 'Rule ayivumelekile.';
