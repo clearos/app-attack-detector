9f9f8f1ef1854677050094c420b6f5189ca715bf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:32:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/it/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'Rilevatore di attacco';
+$lang['attack_detector_app_description'] = 'Rivelatore attacco esegue la scansione del sistema per errori di autenticazione attraverso vari tipi di servizi installati sul vostro sistema. Se viene raggiunta la soglia di fallimento, l&#39;applicazione si blocca il sistema di attacco.';
+$lang['attack_detector_log'] = 'Log';
+$lang['attack_detector_rule'] = 'Regola';
+$lang['attack_detector_rules'] = 'Regole';
+$lang['attack_detector_rule_name'] = 'Nome';
+$lang['attack_detector_ban_time'] = 'Ban Tempo';
+$lang['attack_detector_rule_invalid'] = 'Regola non è valido.';
