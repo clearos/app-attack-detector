84912719951d005a790b62f24130cc6e109a94ce
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:31:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/de/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'Überfallmelder';
+$lang['attack_detector_app_description'] = 'Angriff Detector scannt Ihr System für Authentifizierungsfehler über verschiedene Arten von Diensten auf Ihrem System installiert ist. Wenn die Fehlerschwelle erreicht ist, wird die App das angreifende System blockieren.';
+$lang['attack_detector_log'] = 'Log';
+$lang['attack_detector_rule'] = 'Regel';
+$lang['attack_detector_rules'] = 'Regeln';
+$lang['attack_detector_rule_name'] = 'Name';
+$lang['attack_detector_ban_time'] = 'Ban Zeit';
+$lang['attack_detector_rule_invalid'] = 'Regel ist ungültig.';
