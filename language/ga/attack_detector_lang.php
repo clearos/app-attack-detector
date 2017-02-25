fe6ef85cc5653f4720d06d27928b724bae298e79
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:32:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'Brathadóir ionsaí';
+$lang['attack_detector_app_description'] = 'scans Brathadóir Ionsaí do chóras chun teipeanna fíordheimhnithe feadh cineálacha éagsúla seirbhísí suiteáilte ar do chóras. Má tá an tairseach teip amach, beidh an app bloc an gcóras ionsaí.';
+$lang['attack_detector_log'] = 'Logáil';
+$lang['attack_detector_rule'] = 'riail';
+$lang['attack_detector_rules'] = 'rialacha';
+$lang['attack_detector_rule_name'] = 'Ainm';
+$lang['attack_detector_ban_time'] = 'Ban Am';
+$lang['attack_detector_rule_invalid'] = 'Is Riail neamhbhailí.';
