2295dad39cf235f43f6c23b691cedffaf67cf5e1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:29:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'တိုက်ခိုက် Detector';
+$lang['attack_detector_app_description'] = 'တိုက်ခိုက် Detector သင့် system ပေါ်တွင်တပ်ဆင်ထားန်ဆောင်မှုအမျိုးမျိုးကိုဖြတ်ပြီး authentication ကိုဆုံးရှုံးမှုများအတွက်သင့် system စိစစ်သည်။ ပျက်ကွက်တံခါးခုံသို့ရောက်ရှိလျှင်, app ကိုတိုက်ခိုက်နေတဲ့ system ကို block ပါလိမ့်မယ်။';
+$lang['attack_detector_log'] = 'တုံး';
+$lang['attack_detector_rule'] = 'အုပ်ချုပ်';
+$lang['attack_detector_rules'] = 'စည်းကမ်းများ';
+$lang['attack_detector_rule_name'] = 'Name ကို';
+$lang['attack_detector_ban_time'] = 'ဘန်ကီမွန်းကအချိန်';
+$lang['attack_detector_rule_invalid'] = 'နည်းဥပဒေမမှန်ကန်ဘူး။';
