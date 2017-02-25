413d385437172416e14d19223afb9204ad00aa55
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:33:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'ເຄື່ອງກວດຈັບການໂຈມຕີ';
+$lang['attack_detector_app_description'] = 'ເຄື່ອງກວດຈັບການໂຈມຕີ scans ລະບົບຂອງທ່ານສໍາລັບຄວາມລົ້ມເຫລວການກວດສອບໃນທຸກປະເພດຕ່າງໆຂອງການບໍລິການຕິດຕັ້ງລະບົບຂອງທ່ານ. ຖ້າຫາກວ່າໃກ້ຈະເຂົ້າສູ່ຄວາມລົ້ມເຫຼວແມ່ນບັນລຸໄດ້, app ຈະສະກັດກັ້ນການລະບົບການໂຈມຕີ.';
+$lang['attack_detector_log'] = 'ເຂົ້າສູ່ລະບົບ';
+$lang['attack_detector_rule'] = 'ກົດລະບຽບ';
+$lang['attack_detector_rules'] = 'ກົດລະບຽບ';
+$lang['attack_detector_rule_name'] = 'ຊື່';
+$lang['attack_detector_ban_time'] = 'ບ້ານທີ່ໃຊ້ເວລາ';
+$lang['attack_detector_rule_invalid'] = 'ກົດລະບຽບແມ່ນບໍ່ຖືກຕ້ອງ.';
