799778b2e1fe5eba42519ab0e68b3733e36e88b8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:32:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'アタックディテクタ';
+$lang['attack_detector_app_description'] = 'アタックディテクタは、システムにインストールされているサービスの様々なタイプ間の認証の失敗のためにあなたのシステムをスキャンします。 障害しきい値に達した場合、アプリは攻撃システムをブロックします。';
+$lang['attack_detector_log'] = 'ログ';
+$lang['attack_detector_rule'] = 'ルール';
+$lang['attack_detector_rules'] = 'ルール';
+$lang['attack_detector_rule_name'] = '名';
+$lang['attack_detector_ban_time'] = '潘時間';
+$lang['attack_detector_rule_invalid'] = 'ルールは無効です。';
