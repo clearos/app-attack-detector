d9ca650621f6ef0197cf29b8d8f382661977a4b3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:34:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'हल्ला शोधकर्ता';
+$lang['attack_detector_app_description'] = 'हल्ला शोधकर्ता, प्रणालीवरील प्रतिष्ठापीत सेवा विविध प्रकारच्या ओलांडून प्रमाणीकरण अपयश आपल्या प्रणाली स्कॅन करते. अपयश उंबरठा गाठली आहे, तर, अनुप्रयोग हल्ला प्रणाली अवरोधित करेल.';
+$lang['attack_detector_log'] = 'लॉग';
+$lang['attack_detector_rule'] = 'नियम';
+$lang['attack_detector_rules'] = 'नियम';
+$lang['attack_detector_rule_name'] = 'नाव';
+$lang['attack_detector_ban_time'] = 'बंदी वेळ';
+$lang['attack_detector_rule_invalid'] = 'नियम अवैध आहे.';
