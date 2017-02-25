0f0a87dadcecb1a140f10c557849b7dfe94c5243
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:36:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'ප්රහාරය අනාවරණයක්';
+$lang['attack_detector_app_description'] = 'ප්රහාරය අනාවරණයක් සේවා ඔබේ පද්ධතිය මත ස්ථාපනය විවිධ වර්ගයේ හරහා සත්යාපනය අසාර්ථක ඔබේ පද්ධතිය පරීක්ෂණයට ලක්කරන. අසාර්ථක එළිපත්ත ළඟා තිබේ නම්, එම යෙදුම පහර එල්ල පද්ධතිය අවහිර කරනු ලැබේ.';
+$lang['attack_detector_log'] = 'ලඝු';
+$lang['attack_detector_rule'] = 'නීතිය';
+$lang['attack_detector_rules'] = 'නීති';
+$lang['attack_detector_rule_name'] = 'නම';
+$lang['attack_detector_ban_time'] = 'බෑන් කාල';
+$lang['attack_detector_rule_invalid'] = 'පාලනය වලංගු නැත.';
