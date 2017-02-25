a1776f2fd70e070f84f4da3e476362d93bd9a567
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:29:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'Detector d&#39;atac';
+$lang['attack_detector_app_description'] = 'Detector d&#39;atac explora el sistema d&#39;errors d&#39;autenticació a través de diversos tipus de serveis al vostre sistema. Si s&#39;arriba al llindar de decisió, l&#39;aplicació va a bloquejar el sistema atacar.';
+$lang['attack_detector_log'] = 'Registre';
+$lang['attack_detector_rule'] = 'regla';
+$lang['attack_detector_rules'] = 'regles';
+$lang['attack_detector_rule_name'] = 'Nom';
+$lang['attack_detector_ban_time'] = 'temps Ban';
+$lang['attack_detector_rule_invalid'] = 'Regla no és vàlid.';
