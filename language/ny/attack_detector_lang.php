00fe3e1d565d3b94a9aaa4727afa0d03c55e1dab
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:29:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'kuukira chowunikira';
+$lang['attack_detector_app_description'] = 'Kuukira chowunikira mapanga sikani dongosolo wanu mwa zolephera kutsimikizika kudutsa mitundu yosiyanasiyana ya misonkhano anaika pa dongosolo lanu. Ngati kulephera pakhomo kukufika, app adzakhala ndigwire dongosolo pomenyana.';
+$lang['attack_detector_log'] = 'Log';
+$lang['attack_detector_rule'] = 'Rule';
+$lang['attack_detector_rules'] = 'malamulo';
+$lang['attack_detector_rule_name'] = 'Name';
+$lang['attack_detector_ban_time'] = 'Ban Time';
+$lang['attack_detector_rule_invalid'] = 'Ulamuliro ndi opanda pake.';
