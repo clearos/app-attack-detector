38f5e651b4dc27db57104c50b2b1a55df28612f6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:34:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'Attack Илрүүлэгч';
+$lang['attack_detector_app_description'] = 'Attack Илрүүлэгч нь таны систем дээр суусан үйлчилгээний төрөл бүрийн цаана нэвтрэлт танилт алдаа нь таны системийг шинжилнэ. дутагдал босго хүрсэн бол, апп довтолж системийг хаах болно.';
+$lang['attack_detector_log'] = 'нэвтрэх';
+$lang['attack_detector_rule'] = 'дүрэм';
+$lang['attack_detector_rules'] = 'журам';
+$lang['attack_detector_rule_name'] = 'нэр';
+$lang['attack_detector_ban_time'] = 'Бан цаг';
+$lang['attack_detector_rule_invalid'] = 'Дүрэм хүчин төгөлдөр бус байна.';
