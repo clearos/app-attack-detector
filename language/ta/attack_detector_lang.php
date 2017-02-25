421e0032fc9c63c9b0374923246b4a7c4b03eef0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:38:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'தாக்குதல் கண்டறிபவர்';
+$lang['attack_detector_app_description'] = 'தாக்குதல் டிடெக்டர் உங்கள் கணினியில் நிறுவப்பட்டுள்ள சேவைகள் பல்வேறு வகையான முழுவதும் அங்கீகார தோல்விகளுக்கு உங்கள் கணினியில் ஸ்கேன். தோல்வி வாசலில் அடையும் என்றால், பயன்பாட்டை தாக்கி அமைப்பு தடுக்கும்.';
+$lang['attack_detector_log'] = 'புகுபதிகை';
+$lang['attack_detector_rule'] = 'விதி';
+$lang['attack_detector_rules'] = 'விதிகள்';
+$lang['attack_detector_rule_name'] = 'பெயர்';
+$lang['attack_detector_ban_time'] = 'பான் நேரம்';
+$lang['attack_detector_rule_invalid'] = 'விதி தவறானது.';
