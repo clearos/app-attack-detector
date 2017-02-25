b56dd50f3efe4a963fd2f3606f820d0f183fdc06
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:35:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'ਹਮਲਾ ਡਿਟੈਕਟਰ';
+$lang['attack_detector_app_description'] = 'ਹਮਲਾ ਡਿਟੈਕਟਰ ਨੂੰ ਤੁਹਾਡੇ ਸਿਸਟਮ ਤੇ ਇੰਸਟਾਲ ਸੇਵਾ ਦੇ ਵੱਖ-ਵੱਖ ਕਿਸਮ ਦੇ ਭਰ ਵਿੱਚ ਪ੍ਰਮਾਣਿਕਤਾ ਅਸਫਲਤਾ ਲਈ ਆਪਣੇ ਸਿਸਟਮ ਦੀ ਪੜਤਾਲ. ਅਸਫਲਤਾ ਥ੍ਰੈਸ਼ਹੋਲਡ ਤੇ ਪਹੁੰਚ ਗਿਆ ਹੈ, ਜੇ, ਐਪ ਹਮਲਾ ਸਿਸਟਮ ਨੂੰ ਬਲਾਕ ਕੀਤਾ ਜਾਵੇਗਾ.';
+$lang['attack_detector_log'] = 'ਲਾਗ';
+$lang['attack_detector_rule'] = 'ਨਿਯਮ';
+$lang['attack_detector_rules'] = 'ਨਿਯਮ';
+$lang['attack_detector_rule_name'] = 'ਦਾ ਨਾਮ';
+$lang['attack_detector_ban_time'] = 'ਬਾਨ ਟਾਈਮ';
+$lang['attack_detector_rule_invalid'] = 'ਨਿਯਮ ਗਲਤ ਹੈ.';
