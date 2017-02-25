ef2e841b6bd82b123b1f5526a6fa86d8359df054
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:31:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hi/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'हमला डिटेक्टर';
+$lang['attack_detector_app_description'] = 'हमला डिटेक्टर आपके सिस्टम पर स्थापित सेवाओं के विभिन्न प्रकारों में प्रमाणीकरण विफलताओं के लिए अपने सिस्टम को स्कैन करता है। विफलता सीमा तक पहुँच जाता है, एप्लिकेशन पर हमला कर प्रणाली रोकेंगे।';
+$lang['attack_detector_log'] = 'लॉग';
+$lang['attack_detector_rule'] = 'नियम';
+$lang['attack_detector_rules'] = 'नियम';
+$lang['attack_detector_rule_name'] = 'नाम';
+$lang['attack_detector_ban_time'] = 'बान समय';
+$lang['attack_detector_rule_invalid'] = 'नियम अमान्य है।';
