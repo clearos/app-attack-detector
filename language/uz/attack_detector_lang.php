878ae6c6741c4f2992888b6cfe50737109b9c9b0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:39:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'hujum yassi';
+$lang['attack_detector_app_description'] = 'Hujum yassi tizimi o&#39;rnatilgan xizmatlar turli xil bo&#39;ylab autentifikatsiya muvaffaqiyatsizliklar uchun tizimda ko&#39;zdan kechiradi. etishmovchiligi, pol erishilgan bo&#39;lsa, ilova hujum tizimi to&#39;sadi.';
+$lang['attack_detector_log'] = 'Kirish';
+$lang['attack_detector_rule'] = 'qoida';
+$lang['attack_detector_rules'] = 'qoidalari';
+$lang['attack_detector_rule_name'] = 'ism';
+$lang['attack_detector_ban_time'] = 'Ban Time';
+$lang['attack_detector_rule_invalid'] = 'Qoida bekor.';
