c528d9b9b3726cc21f32e4e8b0a0e64729ec0381
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:30:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cs/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'útok Detector';
+$lang['attack_detector_app_description'] = 'Útok Detektor prohledá váš systém pro selhání ověřování napříč různými typy služeb nainstalovaných na vašem systému. Je-li dosaženo prahu selhání, bude aplikace blokovat útočící systém.';
+$lang['attack_detector_log'] = 'Log';
+$lang['attack_detector_rule'] = 'Pravidlo';
+$lang['attack_detector_rules'] = 'pravidla';
+$lang['attack_detector_rule_name'] = 'název';
+$lang['attack_detector_ban_time'] = 'Ban Time';
+$lang['attack_detector_rule_invalid'] = 'Pravidlo je neplatný.';
