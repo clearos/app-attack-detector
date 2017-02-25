a7e4778c04c679e3a4cd7ae888c56277b6c34f49
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:39:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'حملہ ویکشک';
+$lang['attack_detector_app_description'] = 'حملہ ویکشک آپ کے سسٹم پر نصب کی خدمات کے مختلف اقسام بھر میں تصدیق کی ناکامی کے لئے آپ کے سسٹم کو سکین کرتا ہے. ناکامی دہلیز تک پہنچ جاتا ہے تو، اے پی پی کے حملہ کرنے کے نظام کو بلاک کریں گے.';
+$lang['attack_detector_log'] = 'لاگ';
+$lang['attack_detector_rule'] = 'اصول';
+$lang['attack_detector_rules'] = 'رولز';
+$lang['attack_detector_rule_name'] = 'نام';
+$lang['attack_detector_ban_time'] = 'بان وقت';
+$lang['attack_detector_rule_invalid'] = 'قاعدہ غلط ہے.';
