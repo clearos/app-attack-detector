0eb9aeb9360f5f5a1ee881da68bce714f5c68e06
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:37:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'mashambulizi Detector';
+$lang['attack_detector_app_description'] = 'Mashambulizi Detector scans mfumo wako kwa kushindwa uthibitisho hela aina mbalimbali za huduma imewekwa kwenye mfumo wako. Kama kushindwa kizingiti ni kufikiwa, programu itakuwa kuzuia mfumo kushambulia.';
+$lang['attack_detector_log'] = 'Log';
+$lang['attack_detector_rule'] = 'utawala';
+$lang['attack_detector_rules'] = 'Kanuni';
+$lang['attack_detector_rule_name'] = 'jina';
+$lang['attack_detector_ban_time'] = 'Ban Muda';
+$lang['attack_detector_rule_invalid'] = 'Utawala ni batili.';
