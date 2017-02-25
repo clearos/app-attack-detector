d09a53e3fdc1522d07f6915343de1ddeebe3dae6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:38:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'saldırı Dedektör';
+$lang['attack_detector_app_description'] = 'Saldırı Dedektör sisteminizde yüklü hizmetlerin çeşitli türleri arasında kimlik doğrulama hataları için sisteminizi tarar. başarısızlık eşik aşıldığı takdirde, uygulama saldıran sistemi engeller.';
+$lang['attack_detector_log'] = 'Giriş';
+$lang['attack_detector_rule'] = 'Kural';
+$lang['attack_detector_rules'] = 'kurallar';
+$lang['attack_detector_rule_name'] = 'isim';
+$lang['attack_detector_ban_time'] = 'Ban Zaman';
+$lang['attack_detector_rule_invalid'] = 'Kural geçersiz.';
