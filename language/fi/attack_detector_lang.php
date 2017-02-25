5ca5b430ee9312a3645e24627dd2e7665c9ce4f8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:30:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'Attack Detector';
+$lang['attack_detector_app_description'] = 'Attack Detector skannaa järjestelmän laillisuustarkistusvirheet poikki erityyppisiä palveluja asennettuna järjestelmään. Jos vika raja on saavutettu, sovellus estää hyökkää järjestelmää.';
+$lang['attack_detector_log'] = 'log';
+$lang['attack_detector_rule'] = 'sääntö';
+$lang['attack_detector_rules'] = 'säännöt';
+$lang['attack_detector_rule_name'] = 'Nimi';
+$lang['attack_detector_ban_time'] = 'Ban Time';
+$lang['attack_detector_rule_invalid'] = 'Sääntö on virheellinen.';
