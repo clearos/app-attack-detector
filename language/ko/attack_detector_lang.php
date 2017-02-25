946579420109ba96426c04f9e102d699e1a12be9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:33:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = '공격 감지기';
+$lang['attack_detector_app_description'] = '공격 감지기 시스템에 설치된 서비스의 여러 유형에서 인증 실패에 대한 시스템을 검사합니다. 실패 임계 값에 도달하면, 애플리케이션은 시스템 공격을 차단하는 것이다.';
+$lang['attack_detector_log'] = '로그';
+$lang['attack_detector_rule'] = '규칙';
+$lang['attack_detector_rules'] = '규칙';
+$lang['attack_detector_rule_name'] = '이름';
+$lang['attack_detector_ban_time'] = '반 시간';
+$lang['attack_detector_rule_invalid'] = '규칙이 잘못되었습니다.';
