c0b625f39370acddd9f5b99063c13f7c2f049c80
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:35:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'आक्रमण वेक्षक';
+$lang['attack_detector_app_description'] = 'आक्रमण वेक्षक आफ्नो सिस्टम मा स्थापित सेवाहरू विभिन्न प्रकारका मार्फत प्रमाणीकरण विफलताका लागि आफ्नो सिस्टम स्कैन। छ भने विफलता सीमा पुग्यो, अनुप्रयोगलाई आक्रमण सिस्टम बन्द गर्नेछ।';
+$lang['attack_detector_log'] = 'लग';
+$lang['attack_detector_rule'] = 'नियम';
+$lang['attack_detector_rules'] = 'नियम';
+$lang['attack_detector_rule_name'] = 'नाम';
+$lang['attack_detector_ban_time'] = 'प्रतिबन्ध समय';
+$lang['attack_detector_rule_invalid'] = 'नियम अमान्य छ।';
