1085fe70b710e524ff13c1a828dc5049be37ac46
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:30:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/da/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'Attack Detector';
+$lang['attack_detector_app_description'] = 'Attack Detector scanner dit system for authentication fejl på tværs af forskellige typer af tjenester, der er installeret på dit system. Hvis fejlen tærsklen er nået, vil den app blokere angribende systemet.';
+$lang['attack_detector_log'] = 'Log';
+$lang['attack_detector_rule'] = 'Herske';
+$lang['attack_detector_rules'] = 'Regler';
+$lang['attack_detector_rule_name'] = 'Navn';
+$lang['attack_detector_ban_time'] = 'Ban Time';
+$lang['attack_detector_rule_invalid'] = 'Regel er ugyldig.';
