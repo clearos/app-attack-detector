36e6582acc51813c97ee9a74833debf3786bc651
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:39:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'Detector tấn công';
+$lang['attack_detector_app_description'] = 'Tấn công Detector sẽ quét hệ thống của bạn cho những thất bại xác thực trên các loại khác nhau của các dịch vụ được cài đặt trên hệ thống của bạn. Nếu ngưỡng thất bại được đạt tới, các ứng dụng sẽ chặn các hệ thống tấn công.';
+$lang['attack_detector_log'] = 'Đăng nhập';
+$lang['attack_detector_rule'] = 'Qui định';
+$lang['attack_detector_rules'] = 'Quy tắc';
+$lang['attack_detector_rule_name'] = 'Tên';
+$lang['attack_detector_ban_time'] = 'Ban Time';
+$lang['attack_detector_rule_invalid'] = 'Quy tắc là không hợp lệ.';
