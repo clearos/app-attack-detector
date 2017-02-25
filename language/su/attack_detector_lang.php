c5a9c88639f524a3390853c289d89925bbc34c14
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:37:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'serangan detektor';
+$lang['attack_detector_app_description'] = 'Serangan detektor nyeken sistim Anjeun pikeun gagal auténtikasi sakuliah rupa-rupa layanan dipasang dina sistim Anjeun. Lamun bangbarung kagagalan ieu kahontal, app bakal meungpeuk sistim nyerang.';
+$lang['attack_detector_log'] = 'log';
+$lang['attack_detector_rule'] = 'aturan';
+$lang['attack_detector_rules'] = 'aturan';
+$lang['attack_detector_rule_name'] = 'nami';
+$lang['attack_detector_ban_time'] = 'Ban Time';
+$lang['attack_detector_rule_invalid'] = 'Aturan anu sah.';
