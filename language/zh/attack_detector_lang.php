02d4c4a07c5cf477fab71aa7b7d3072264aa9da2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:29:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = '攻击检测';
+$lang['attack_detector_app_description'] = '攻击检测扫描您的跨各类系统中安装的服务的身份验证失败制度。 如果达到了故障阈值，应用程序将阻止攻击的系统。';
+$lang['attack_detector_log'] = '日志';
+$lang['attack_detector_rule'] = '规则';
+$lang['attack_detector_rules'] = '规则';
+$lang['attack_detector_rule_name'] = '名称';
+$lang['attack_detector_ban_time'] = '班时间';
+$lang['attack_detector_rule_invalid'] = '规则是无效的。';
