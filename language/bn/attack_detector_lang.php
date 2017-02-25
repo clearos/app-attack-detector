05d71c1ed160820bae07c9b973f2e54cccccc490
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:29:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'আক্রমণ আবিষ্কারক';
+$lang['attack_detector_app_description'] = 'আক্রমণ আবিষ্কারক আপনার সিস্টেমে ইনস্টল পরিষেবার বিভিন্ন ধরনের জুড়ে প্রমাণীকরণ ব্যর্থতা জন্য আপনার সিস্টেম স্ক্যান. ব্যর্থতা প্রান্তিক পৌঁছেছেন হয়, তাহলে অ্যাপ্লিকেশন আক্রমণ সিস্টেম ব্লক হবে.';
+$lang['attack_detector_log'] = 'লগিন';
+$lang['attack_detector_rule'] = 'নিয়ম';
+$lang['attack_detector_rules'] = 'বিধি';
+$lang['attack_detector_rule_name'] = 'নাম';
+$lang['attack_detector_ban_time'] = 'বান সময়';
+$lang['attack_detector_rule_invalid'] = 'রুল অকার্যকর.';
