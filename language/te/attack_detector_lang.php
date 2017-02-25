4c2ff6f8d0e3a60f53fdd5cc8537dc40f2fb81b6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:38:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'దాడి డిటెక్టర్';
+$lang['attack_detector_app_description'] = 'దాడి డిటెక్టర్ మీ సిస్టమ్లో ఇన్స్టాల్ సేవలు వివిధ రకాల ధృవీకరణ వైఫల్యాలు మీ సిస్టమ్ స్కాన్ చేస్తుంది. అయితే వైఫల్యం ప్రవేశ చేరుకున్న, అనువర్తనం దాడి వ్యవస్థ నిరోధించగలరు.';
+$lang['attack_detector_log'] = 'లోనికి ప్రవేశించండి';
+$lang['attack_detector_rule'] = 'రూల్';
+$lang['attack_detector_rules'] = 'రూల్స్';
+$lang['attack_detector_rule_name'] = 'పేరు';
+$lang['attack_detector_ban_time'] = 'బాన్ సమయం';
+$lang['attack_detector_rule_invalid'] = 'రూల్ చెల్లదు.';
