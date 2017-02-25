6f8ffd0422b884d63abda907da843e98a62fda23
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:28:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'Attack Detector';
+$lang['attack_detector_app_description'] = 'Attack Detector sistem yüklü xidmətlərin müxtəlif növləri üzrə identifikasiyası uğursuzluqlar üçün sistem tarar. uğursuzluq ərəfəsində əldə varsa, app hücum sistemi blok olacaq.';
+$lang['attack_detector_log'] = 'Log';
+$lang['attack_detector_rule'] = 'qayda';
+$lang['attack_detector_rules'] = 'Qaydalar';
+$lang['attack_detector_rule_name'] = 'ad';
+$lang['attack_detector_ban_time'] = 'Ban Time';
+$lang['attack_detector_rule_invalid'] = 'Rule yanlışdır.';
