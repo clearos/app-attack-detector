cc301b4759d42ed42cbb078efa9cec60c7c3fc4d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:35:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'Pengesan Attack';
+$lang['attack_detector_app_description'] = 'Serangan Pengesan mengimbas sistem anda untuk kegagalan pengesahan merentasi pelbagai jenis perkhidmatan yang dipasang pada sistem anda. Jika ambang kegagalan dicapai, app akan menyekat sistem menyerang.';
+$lang['attack_detector_log'] = 'Log';
+$lang['attack_detector_rule'] = 'peraturan';
+$lang['attack_detector_rules'] = 'peraturan';
+$lang['attack_detector_rule_name'] = 'Nama';
+$lang['attack_detector_ban_time'] = 'Ban Masa';
+$lang['attack_detector_rule_invalid'] = 'Peraturan tidak sah.';
