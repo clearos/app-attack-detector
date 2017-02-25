38489ae8699470388e2e9c3a4a9753eb5e6eebe3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:38:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'ตรวจจับการโจมตี';
+$lang['attack_detector_app_description'] = 'ตรวจจับการโจมตีสแกนระบบของคุณสำหรับความล้มเหลวในการตรวจสอบในทุกประเภทของบริการต่าง ๆ ที่ติดตั้งบนระบบของคุณ หากเกณฑ์ความล้มเหลวถึง app ที่จะป้องกันการโจมตีระบบ';
+$lang['attack_detector_log'] = 'เข้าสู่ระบบ';
+$lang['attack_detector_rule'] = 'กฎ';
+$lang['attack_detector_rules'] = 'กฎระเบียบ';
+$lang['attack_detector_rule_name'] = 'ชื่อ';
+$lang['attack_detector_ban_time'] = 'บ้านเวลา';
+$lang['attack_detector_rule_invalid'] = 'กฎข้อที่ไม่ถูกต้อง';
