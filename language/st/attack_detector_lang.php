92ba916b5b26e308425414ae209082eb6ed0d108
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:37:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'tlhaselo semilolog';
+$lang['attack_detector_app_description'] = 'Tlhaselo semilolog e skena hao tsamaiso bakeng sa ho hloleha ha netefatso ka mose mefuta e sa tšoaneng ea ditshebeletso hlomamisa ya hao tsamaiso. Haeba ho hlōleha monyakong o fihla, app e tla thibela tsamaiso hlasela.';
+$lang['attack_detector_log'] = 'Log';
+$lang['attack_detector_rule'] = 'Rule';
+$lang['attack_detector_rules'] = 'Rules';
+$lang['attack_detector_rule_name'] = 'Name';
+$lang['attack_detector_ban_time'] = 'Ban Time';
+$lang['attack_detector_rule_invalid'] = 'Rule ha o sebetse.';
