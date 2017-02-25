acd5911ba17f46db9e963d8c19ff6e0dab29f208
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:31:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'Attack Gano';
+$lang['attack_detector_app_description'] = 'Attack Gano sikanin your tsarin Tantance kalmar sirri kasawa fadin daban-daban iri ayyuka shigar a kan tsarin. Idan gazawar bakin kofa ne kai, da app zai toshe kai hare hare da tsarin.';
+$lang['attack_detector_log'] = 'log';
+$lang['attack_detector_rule'] = 'Rule';
+$lang['attack_detector_rules'] = 'dokokin';
+$lang['attack_detector_rule_name'] = 'name';
+$lang['attack_detector_ban_time'] = 'Ban Time';
+$lang['attack_detector_rule_invalid'] = 'Rule ba daidai ba ne.';
