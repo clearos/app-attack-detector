194b63c4b1db9ab906fa3dc1a9f8d5725f38b5ce
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:34:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'fanafihana Detector';
+$lang['attack_detector_app_description'] = 'Fanafihana Detector ary mitazana ny rafitra ho an&#39;ny fanamarinana tsy fahombiazana isan-karazany manerana ny tolotra napetraka amin&#39;ny rafitra. Raha toa ny tsy fahombiazan&#39;ny tokonam-baravarana no tonga, dia hanakana ny fampiharana ny rafitra nanafika.';
+$lang['attack_detector_log'] = 'Log';
+$lang['attack_detector_rule'] = 'fitsipika';
+$lang['attack_detector_rules'] = 'fitsipika';
+$lang['attack_detector_rule_name'] = 'Name';
+$lang['attack_detector_ban_time'] = 'Ban Time';
+$lang['attack_detector_rule_invalid'] = 'Fitsipika dia tsy manan-kery.';
