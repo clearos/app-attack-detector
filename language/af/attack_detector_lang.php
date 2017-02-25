e07b2117e2d30080023adf23572c76cc5f9787f7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:28:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'aanval Aanwyser';
+$lang['attack_detector_app_description'] = 'Aanval Aanwyser skanderings jou stelsel vir verifikasie mislukkings oor verskillende tipes dienste op jou stelsel geïnstalleer. As die mislukking drempelwaarde bereik, sal die app die aanvallende stelsel blok.';
+$lang['attack_detector_log'] = 'Meld';
+$lang['attack_detector_rule'] = 'reël';
+$lang['attack_detector_rules'] = 'reëls';
+$lang['attack_detector_rule_name'] = 'naam';
+$lang['attack_detector_ban_time'] = 'Ban Tyd';
+$lang['attack_detector_rule_invalid'] = 'Reël is ongeldig.';
