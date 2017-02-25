512dbe17f55a35d24b91c2c9e1d2e364237fe354
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:38:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'детектори ҳамла';
+$lang['attack_detector_app_description'] = 'Детектори ҳамла Технологияи аз системаи шумо барои нокомиҳои аслӣ дар саросари намудҳои гуногуни хизматрасонии насб дар системаи шумо. Агар остонаи нокомии расида аст, барнома хоҳанд системаи ҳамла маҳкам.';
+$lang['attack_detector_log'] = 'Русский';
+$lang['attack_detector_rule'] = 'Ќоидаи';
+$lang['attack_detector_rules'] = 'Қоидаҳои';
+$lang['attack_detector_rule_name'] = 'ном';
+$lang['attack_detector_ban_time'] = 'Бан Time';
+$lang['attack_detector_rule_invalid'] = 'Қоидаи нодуруст аст.';
