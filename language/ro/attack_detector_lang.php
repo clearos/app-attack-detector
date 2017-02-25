bf1ac2561e4fef847d5380da4ab9b4f5348e2528
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:36:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'Detector de atac';
+$lang['attack_detector_app_description'] = 'Detector de atac scanează sistemul pentru erori de autentificare în diferite tipuri de servicii instalate pe sistemul dumneavoastră. În cazul în care se atinge pragul de eșec, aplicația va bloca sistemul ataca.';
+$lang['attack_detector_log'] = 'Buturuga';
+$lang['attack_detector_rule'] = 'Regulă';
+$lang['attack_detector_rules'] = 'normele';
+$lang['attack_detector_rule_name'] = 'Nume';
+$lang['attack_detector_ban_time'] = 'Timp ban';
+$lang['attack_detector_rule_invalid'] = 'Regula este nevalid.';
