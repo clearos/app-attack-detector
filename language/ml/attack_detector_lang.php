0c47d96e483c248320f4d1bd2acd99ac04ed2936
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:34:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'ആക്രമണം ഡിറ്റക്റ്റർ';
+$lang['attack_detector_app_description'] = 'ആക്രമണം ഡിറ്റക്റ്റർ സിസ്റ്റത്തിൽ ഇൻസ്റ്റോൾ സേവനങ്ങൾ വിവിധ തരം ആധികാരികത പരാജയങ്ങൾ നിങ്ങളുടെ സിസ്റ്റം സ്കാൻ ചെയ്യും. പരാജയം ഉമ്മരപ്പടി എത്തിച്ചേരുമ്പോൾ, ആപ്ലിക്കേഷൻ ആക്രമിക്കുന്ന സിസ്റ്റം തടയും.';
+$lang['attack_detector_log'] = 'പ്രവേശിക്കുക';
+$lang['attack_detector_rule'] = 'ചട്ടം';
+$lang['attack_detector_rules'] = 'നിയമങ്ങൾ';
+$lang['attack_detector_rule_name'] = 'പേര്';
+$lang['attack_detector_ban_time'] = 'ബാൻ സമയം';
+$lang['attack_detector_rule_invalid'] = 'റൂൾ അസാധുവാണ്.';
