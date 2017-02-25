a4a4b53d1eedda48184475d873beb000c1287753
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:30:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'atako Detektilo';
+$lang['attack_detector_app_description'] = 'Atako Detektilo escanea via sistemo por aŭtentokontrolo fiaskoj trans diversaj tipoj de servoj instalitaj en via sistemo. Se la fiasko sojlo estas atingita, la app blokos la atakanta sistemo.';
+$lang['attack_detector_log'] = 'ŝtipo';
+$lang['attack_detector_rule'] = 'regulo';
+$lang['attack_detector_rules'] = 'reguloj';
+$lang['attack_detector_rule_name'] = 'nomo';
+$lang['attack_detector_ban_time'] = 'malpermeso Tempo';
+$lang['attack_detector_rule_invalid'] = 'Regulo estas nevalida.';
