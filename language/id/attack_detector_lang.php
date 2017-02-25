b44e49dc81bd40cdb28e2d4a76bb2a02f8ca2c8e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:32:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'serangan Detector';
+$lang['attack_detector_app_description'] = 'Serangan Detector scan sistem anda untuk kegagalan otentikasi di berbagai jenis layanan diinstal pada sistem Anda. Jika ambang kegagalan tercapai, aplikasi akan memblokir sistem menyerang.';
+$lang['attack_detector_log'] = 'Log';
+$lang['attack_detector_rule'] = 'Aturan';
+$lang['attack_detector_rules'] = 'aturan';
+$lang['attack_detector_rule_name'] = 'Nama';
+$lang['attack_detector_ban_time'] = 'Ban Waktu';
+$lang['attack_detector_rule_invalid'] = 'Aturan tidak valid.';
