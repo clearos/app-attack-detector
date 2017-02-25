28874606f0e3e286ba9893273f997ac2ec8762df
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:34:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/attack_detector_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['attack_detector_app_name'] = 'Attack Detector';
+$lang['attack_detector_app_description'] = 'Attack Detector skenē jūsu sistēmas autentifikācijas neveiksmēm visā dažādu pakalpojumu instalēta jūsu sistēmā. Ja neapmierinoši slieksnis tiek sasniegts, app bloķēs uzbrūk sistēmu.';
+$lang['attack_detector_log'] = 'log';
+$lang['attack_detector_rule'] = 'noteikums';
+$lang['attack_detector_rules'] = 'noteikumi';
+$lang['attack_detector_rule_name'] = 'nosaukums';
+$lang['attack_detector_ban_time'] = 'Ban laiks';
+$lang['attack_detector_rule_invalid'] = 'Noteikums ir nederīgs.';
